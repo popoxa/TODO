34ef1cd49dc36dfab21456cce670ed926ca14b3a
popoxa <oper12052@gmail.com>
2019-11-23 19:02:50 +0500

    Commit 4

--- TODO_list/views.py
+++ TODO_list/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Case
+
 def index(request):
-    context = {}
+    todo_list = Case.objects.order_by('id')
+    context = {'todo_list' : todo_list}
     return render(request,'TODO/index.html', context)
 # Create your views here.
